60e496d5de61ce5aeb97310c3da878a6334090bb
Kumar Ayush <krayush25@gmail.com>
2023-07-28 17:29:43 +0530

    biteSpeed identify API

---
+++ src/main/java/com/bitespeed/backend/BackendApplication.java
@@ -0,0 +1,34 @@
+package com.bitespeed.backend;
+
+import com.bitespeed.backend.entity.Contact;
+import com.bitespeed.backend.service.ContactService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initialCreate(ContactService contactService) {
+		return (args) -> {
+//			Contact contact1 = new Contact("123456","lorraine@hillvalley.edu",null,"primary","2023-04-01 00:00:00"
+//					,"2023-04-01 00:00:00", null);
+//			contactService.addContact(contact1);
+//
+//			Contact contact2 = new Contact("123456","mcfly@hillvalley.edu",1,"secondary","2023-04-20 05:30:00"
+//					,"2023-04-01 00:00:00", null);
+//			contactService.addContact(contact2);
+
+			//Example 2
+//			Contact contact3 = new Contact(23,"123456","mcfly@hillvalley.edu",1,"secondary","2023-04-20 05:30:00"
+//					,"2023-04-01 00:00:00", null);
+//			contactService.addContact(contact2);
+		};
+	}
+}
